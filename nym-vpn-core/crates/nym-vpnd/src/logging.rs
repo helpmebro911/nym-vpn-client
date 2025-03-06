6e4a5e25aee2e34d1d90f0ef3815cb0919d46bad
Jon Häggblad <jon.haggblad@gmail.com>
2025-03-06 14:11:02 +0100

    wip

--- nym-vpn-core/crates/nym-vpnd/src/logging.rs
+++ nym-vpn-core/crates/nym-vpnd/src/logging.rs
@@ -17,7 +17,7 @@ pub struct Options {
     pub enable_stdout_log: bool,
 }
 
-static INFO_CRATES: &[&str; 16] = &[
+static INFO_CRATES: &[&str] = &[
     "hyper",
     "netlink_proto",
     "hickory_proto",
@@ -25,15 +25,15 @@ static INFO_CRATES: &[&str; 16] = &[
     "hyper_util",
     "h2",
     "rustls",
-    "nym_statistics_common",
-    "nym_client_core",
-    "nym_vpn_account_controller",
-    "nym_vpn_store",
-    "nym_vpn_api_client::jwt",
-    "nym_sphinx_chunking",
-    "nym_sphinx::preparer",
-    "nym_authenticator_client",
-    "nym_task::manager",
+    //"nym_statistics_common",
+    //"nym_client_core",
+    //"nym_vpn_account_controller",
+    //"nym_vpn_store",
+    //"nym_vpn_api_client::jwt",
+    //"nym_sphinx_chunking",
+    //"nym_sphinx::preparer",
+    //"nym_authenticator_client",
+    //"nym_task::manager",
 ];
 
 static WARN_CRATES: &[&str; 1] = &["hickory_server"];
