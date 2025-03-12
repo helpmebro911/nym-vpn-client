5ed8e7e95d4a9f01ade566d18ecfc9acfe717d1b
Jon Häggblad <jon.haggblad@gmail.com>
2025-03-12 13:42:26 +0100

    wip

--- nym-vpn-core/crates/nym-ip-packet-client/src/listener.rs
+++ nym-vpn-core/crates/nym-ip-packet-client/src/listener.rs
@@ -33,7 +33,8 @@ pub enum IprListenerError {
 
 impl IprListener {
     pub fn new() -> Self {
-        let decoder = MultiIpPacketCodec::new(nym_ip_packet_requests::codec::BUFFER_TIMEOUT);
+        // let decoder = MultiIpPacketCodec::new(nym_ip_packet_requests::codec::BUFFER_TIMEOUT);
+        let decoder = MultiIpPacketCodec::new();
         Self { decoder }
     }
 
